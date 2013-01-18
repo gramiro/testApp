2264056e720a030d621e62ede252ff99bc4b444a
Zarph <ramiro@leapcommerce.com>
2013-01-18 17:14:16 -0300

    Initial Commit

---
+++ testApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Ramiro Guerrero on 18/01/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
